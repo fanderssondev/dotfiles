ef2082c6184e9d366907efa5ac51898cb52a6b62
Fredrik Andersson <fredrikanderssondev@gmail.com>
2026-01-06 16:03:58 +0100

    add/vim bash template

---
+++ .vim/templates/bash_template.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 
+#  ________    _       
+# |_   __  |  / \        Fredrik Andersson 
+#   | |_ \_| / _ \       https://github.com/fanderssondev
+#   |  _|   / ___ \      <DATE>
+#  _| |_  _/ /   \ \_  
+# |_____||____| |____| 
+#
+# 
